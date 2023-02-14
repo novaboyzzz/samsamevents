8411e0d24bd1b1cf1cd11a417337fd5218647190
artzoo <armin.tzoulakis@gmail.com>
2023-02-14 11:24:15 +0100

    database basic

---
+++ samsam-events/database/migrations/2023_02_14_101419_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->longText('name');
+            $table->integer('price');
+            $table->longText('description');
+            $table->enum('category', ['springkussen', 'challengekussen', 'stormbaan']);
+            $table->longText('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
