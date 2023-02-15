171a634f11479b1c0cff42d7fe933c9cc7fc58eb
artzoo <armin.tzoulakis@gmail.com>
2023-02-15 13:46:14 +0100

    database-basic

---
+++ database/migrations/2023_02_15_115327_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->longtext('name');
+            $table->longtext('discription');
+            $table->integer('price');
+            $table->enum('category',['challengekussen','stormbaan','springkussen']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
