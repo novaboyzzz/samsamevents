2d365eb4ef11ea46045b7e0dfd9ef31ca570f8ab
intConvict <joostvangent911@gmail.com>
2023-02-15 15:27:45 +0100

    FUCKING FINALLY

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id='root'>
+            
+        </div>    
+            <!---------------------------------------- script ------------------------------------------>
+            @vite('resources/js/index.jsx')
+    </body>
+</html>
+ 
