bb3ed1a20e44e48a38925f6848bf9e3fd90708a7
intConvict <joostvangent911@gmail.com>
2023-04-17 16:18:34 +0200

    fix: about me text finish

--- resources/js/about_me-component.jsx
+++ resources/js/about_me-component.jsx
@@ -4,6 +4,9 @@ import React from "react";
 //scss import
 import "../scss/About_me-component.scss";
 
+//component import
+import about_Image from "../../public/images/about-image.png";
+
 //create function
 function About() {
   return(
@@ -13,9 +16,12 @@ function About() {
           <h2>
             lorem ipsum
           </h2>
-          <p>
-          dolor sit amet consectetur adipisicing elit. Maxime ollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamquia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit.
-          </p>
+          <div className="about-me_wrapper__inner__textholder">
+            <img src={about_Image} width="500px"/>
+            <p>
+            dolor sit amet consectetur adipisicing elit. Maxime ollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamquia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit.
+            </p>
+          </div>
         </div>
       </div>
     </>
